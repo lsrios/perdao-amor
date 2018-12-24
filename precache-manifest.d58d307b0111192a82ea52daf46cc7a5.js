8161bdb41d73e451cc7a5c231fd1b61a61e29e47
lsrios <lsrios01@gmail.com>
2018-12-24 16:04:51 -0200

    Updates

---
+++ precache-manifest.d58d307b0111192a82ea52daf46cc7a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "db171ae9f5119d3cf5bf",
+    "url": "/perdao-amor/static/css/main.11a58b1c.chunk.css"
+  },
+  {
+    "revision": "db171ae9f5119d3cf5bf",
+    "url": "/perdao-amor/static/js/main.db171ae9.chunk.js"
+  },
+  {
+    "revision": "bf91f9921acc95b27d75",
+    "url": "/perdao-amor/static/css/1.fc0a4c37.chunk.css"
+  },
+  {
+    "revision": "bf91f9921acc95b27d75",
+    "url": "/perdao-amor/static/js/1.bf91f992.chunk.js"
+  },
+  {
+    "revision": "e273f7356c5e955f1f18",
+    "url": "/perdao-amor/static/js/runtime~main.e273f735.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/perdao-amor/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/perdao-amor/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/perdao-amor/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/perdao-amor/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/perdao-amor/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f067517400b404a84dca56fefbb77018",
+    "url": "/perdao-amor/index.html"
+  }
+];
